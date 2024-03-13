ab0857333750a71f9e44e8a3dff709176cb18627
olavolicursi <69656251+olavolicursi@users.noreply.github.com>
2024-03-13 08:58:11 -0300

    login page

--- app.py
+++ app.py
@@ -3,29 +3,34 @@ from actors.page import show_actors
 from genres.page import show_genres
 from movies.page import show_movies
 from reviews.page import show_reviews
+from login.page import show_login
+
 
 def main():
-    st.title('Flix App')
+    if 'token' not in st.session_state:
+        show_login()
+    else:
+        st.title('Flix App')
 
-    menu_option = st.sidebar.selectbox(
-        'Selecione uma opção',
-        ['Inicio', 'Gêneros', 'Atores/Atrizes', 'Filmes', 'Avaliações']
-    )
+        menu_option = st.sidebar.selectbox(
+            'Selecione uma opção',
+            ['Inicio', 'Gêneros', 'Atores/Atrizes', 'Filmes', 'Avaliações']
+        )
 
-    if menu_option == 'Inicio':
-        st.write('Inicio')
+        if menu_option == 'Inicio':
+            st.write('Inicio')
 
-    if menu_option == 'Gêneros':
-        show_genres()
+        if menu_option == 'Gêneros':
+            show_genres()
 
-    if menu_option == 'Atores/Atrizes':
-        show_actors()
+        if menu_option == 'Atores/Atrizes':
+            show_actors()
 
-    if menu_option == 'Filmes':
-        show_movies()
+        if menu_option == 'Filmes':
+            show_movies()
 
-    if menu_option == 'Avaliações':
-        show_reviews()
+        if menu_option == 'Avaliações':
+            show_reviews()
 
 if __name__ == '__main__':
     main()
